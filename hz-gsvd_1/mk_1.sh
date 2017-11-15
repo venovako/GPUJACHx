d92d41be7fec4843c7a5a313c79a1e27e5bc5f3c
Vedran Novakovic <venovako@venovako.eu>
2017-11-15 12:47:37 +0000

    small fixes

--- hz-gsvd_1/mk_1.sh
+++ hz-gsvd_1/mk_1.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-make GPU_ARCH=sm_$1 NDEBUG=$2 HDF5=/usr/local HDF5_FULL="mpi,z" HOST_FLAGS="-ffp-contract=on,-integrated-as" CVG=1 $3
+# E.g., ANIMATE=1 HDF5_FULL="mpi,z"
+make GPU_ARCH=sm_$1 NDEBUG=$2 HOST_FLAGS="-ffp-contract=on,-integrated-as" CVG=1 $3
