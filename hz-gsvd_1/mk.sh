9ccfe94fa4f705dc7902b606fa90d9a9ffd4f92b
Vedran Novakovic <venovako@venovako.eu>
2018-06-10 00:32:52 +0200

    Move hz-gsvd_1 to https://github.com/venovako/GPUHZGSVD repository.

--- hz-gsvd_1/mk.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-make GPU_ARCH=sm_$1 NDEBUG=$2 $3
--- hz-gsvd_1/mk_1.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# E.g., ANIMATE=1 HDF5_FULL="mpi,z"
-make GPU_ARCH=sm_$1 NDEBUG=$2 HOST_FLAGS="-ffp-contract=on,-integrated-as" CVG=1 $3
