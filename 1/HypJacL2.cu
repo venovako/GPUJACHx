442872b6948608ef6692b8586bd99a985b441d41
Vedran Novakovic <venovako@venovako.eu>
2018-09-25 22:04:57 +0200

    Add cudaDeviceSynchronize() before each block-pstep.

--- 1/HypJacL2.cu
+++ 1/HypJacL2.cu
@@ -145,6 +145,9 @@ hypJacL2
     *cvg_dat = 0ul;
 
     for (unsigned blk_stp = 0u; blk_stp < STRAT1_STEPS; ++blk_stp) {
+      if (blk_stp)
+        CUDA_CALL(cudaDeviceSynchronize());
+
       if (definite)
         defJac1(blk_stp, definite);
       else
