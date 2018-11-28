94f234d874ca1d08dcb7ad49b7636b2fc34a787b
Vedran Novakovic <venovako@venovako.eu>
2018-11-28 21:02:31 +0100

    code cleanup

--- 1/main.cu
+++ 1/main.cu
@@ -7,87 +7,49 @@
 #include "hdf5_helper.hpp"
 #include "my_utils.hpp"
 
-struct CmdArgs {
-  char *exe;
-  char *dev;
-  char *sdy;
-  char *snp0;
-  char *snp1;
-  char *alg;
-  char *h5f;
-  char *h5g;
-  char *h5r;
-
-  CmdArgs(const int argc, char *const argv[]) throw()
-    : exe(static_cast<char*>(NULL)),
-      dev(static_cast<char*>(NULL)),
-      sdy(static_cast<char*>(NULL)),
-      snp0(static_cast<char*>(NULL)),
-      snp1(static_cast<char*>(NULL)),
-      alg(static_cast<char*>(NULL)),
-      h5f(static_cast<char*>(NULL)),
-      h5g(static_cast<char*>(NULL)),
-      h5r(static_cast<char*>(NULL))
-  {
-    if (1 > argc)
-      DIE("argc <= 0");
-    if (!argv)
-      DIE("NULL argv");
-
-    if ((8 > argc) || (9 < argc)) {
-      (void)fprintf(stderr, "%s DEV SDY SNP0 SNP1 ALG H5F H5G [H5R]\n", argv[0]);
-      exit(EXIT_FAILURE);
-    }
-
-    exe = argv[0];
-    dev = argv[1];
-    sdy = argv[2];
-    snp0 = argv[3];
-    snp1 = argv[4];
-    alg = argv[5];
-    h5f = argv[6];
-    h5g = argv[7];
-    h5r = ((9 == argc) ? argv[8] : static_cast<char*>(NULL));
-  }
-};
-
 int main(int argc, char *argv[])
 {
-  int ret = EXIT_SUCCESS;
+  if ((8 > argc) || (9 < argc)) {
+    (void)fprintf(stderr, "%s DEV SDY SNP0 SNP1 ALG H5F H5G [H5R]\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+
+  const char *const ca_exe = argv[0];
+  const char *const ca_dev = argv[1];
+  const char *const ca_sdy = argv[2];
+  const char *const ca_snp0 = argv[3];
+  const char *const ca_snp1 = argv[4];
+  const char *const ca_alg = argv[5];
+  const char *const ca_h5f = argv[6];
+  const char *const ca_h5g = argv[7];
+  const char *const ca_h5r = ((9 == argc) ? argv[8] : static_cast<const char*>(NULL));
 
   unsigned idadim[HYPJAC_IDADIM_SIZE] = { 0u };
   hid_t fid = static_cast<hid_t>(-1), gid = static_cast<hid_t>(-1);
   size_t ldA = static_cast<size_t>(0u), m = static_cast<size_t>(0u), n = static_cast<size_t>(0u);
 
-  CmdArgs ca(argc, argv);
-
   const unsigned n0 = (HYPJACL1_NCOLB << 1u);
-  const unsigned n1 = (static_cast<unsigned>(atoi(ca.h5g)) + HYPJACL1_NCOLB - 1u) / HYPJACL1_NCOLB;
-  init_strats(ca.sdy, ca.snp0, n0, ca.snp1, n1);
+  const unsigned n1 = (static_cast<unsigned>(atoi(ca_h5g)) + HYPJACL1_NCOLB - 1u) / HYPJACL1_NCOLB;
+  init_strats(ca_sdy, ca_snp0, n0, ca_snp1, n1);
 
-  const int dev = atoi(ca.dev);
-#ifndef NDEBUG
-  const int dcc =
-#else // NDEBUG
-  (void)
-#endif // !NDEBUG
-    configureGPU(dev);
-#ifndef NDEBUG
-  (void)fprintf(stdout, "Device %d [CC %d] initialized.\n", dev, dcc);
-  (void)fflush(stdout);
-#endif // !NDEBUG
+  const int dev = atoi(ca_dev);
+  const int dcc = configureGPU(dev);
+  if (dcc < 30) {
+    (void)snprintf(err_msg, err_msg_size, "Device %d has CC %d < 30", dev, dcc);
+    DIE(err_msg);
+  }
 
-  const unsigned routine = static_cast<unsigned>(atoi(ca.alg));
+  const unsigned routine = static_cast<unsigned>(atoi(ca_alg));
 
-  if (!fexist(ca.h5f)) {
-    (void)snprintf(err_msg, err_msg_size, "non-existent H5F(%s)", ca.h5f);
+  if (!fexist(ca_h5f)) {
+    (void)snprintf(err_msg, err_msg_size, "non-existent H5F(%s)", ca_h5f);
     DIE(err_msg);
   }
 
   HDF5_CALL(H5open());
 
-  HDF5_CALL((fid = H5Fopen(ca.h5f, H5F_ACC_RDONLY, H5P_DEFAULT)));
-  HDF5_CALL((gid = H5Gopen2(fid, ca.h5g, H5P_DEFAULT)));
+  HDF5_CALL((fid = H5Fopen(ca_h5f, H5F_ACC_RDONLY, H5P_DEFAULT)));
+  HDF5_CALL((gid = H5Gopen2(fid, ca_h5g, H5P_DEFAULT)));
 
   HDF5_CALL(H5LTread_dataset_int(gid, "IDADIM", reinterpret_cast<int*>(idadim)));
 
@@ -128,10 +90,10 @@ int main(int argc, char *argv[])
   unsigned glbSwp = 0u;
   unsigned Long glb_s = MkLong(0u), glb_b = MkLong(0u);
   double timing[4] = { -0.0, -0.0, -0.0, -0.0 };
-  ret = hypJacL2(routine, nrow, ncol, nplus, hG, ldhG, hV, ldhV, hD, &glbSwp, &glb_s, &glb_b, timing);
+  int ret = hypJacL2(routine, nrow, ncol, nplus, hG, ldhG, hV, ldhV, hD, &glbSwp, &glb_s, &glb_b, timing);
 
   if (ret)
-    (void)fprintf(stderr, "%s: error %d\n", ca.exe, ret);
+    (void)fprintf(stderr, "%s: error %d\n", ca_exe, ret);
   else {
     (void)fprintf(stdout, "GLB_ROT_S(%20" FmtLong "u), GLB_ROT_B(%20" FmtLong "u)\n", glb_s, glb_b);
     (void)fflush(stdout);
@@ -139,17 +101,17 @@ int main(int argc, char *argv[])
     (void)fflush(stdout);
   }
 
-  if (ca.h5r) {
-    HDF5_CALL(fid = fexist(ca.h5r) ?
-              H5Fopen(ca.h5r, H5F_ACC_RDWR, H5P_DEFAULT) :
-              H5Fcreate(ca.h5r, H5F_ACC_EXCL, H5P_DEFAULT, H5P_DEFAULT));
-    HDF5_CALL(gid = H5Gcreate2(fid, ca.h5g, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT));
+  if (ca_h5r) {
+    HDF5_CALL(fid = fexist(ca_h5r) ?
+              H5Fopen(ca_h5r, H5F_ACC_RDWR, H5P_DEFAULT) :
+              H5Fcreate(ca_h5r, H5F_ACC_EXCL, H5P_DEFAULT, H5P_DEFAULT));
+    HDF5_CALL(gid = H5Gcreate2(fid, ca_h5g, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT));
 
     idadim[0] = ldhG;
     hsize_t dims[2] = { static_cast<hsize_t>(HYPJAC_IDADIM_SIZE), static_cast<hsize_t>(1u) };
     HDF5_CALL(H5LTmake_dataset_int(gid, "IDADIM", 1, dims, reinterpret_cast<int*>(idadim)));
 
-    int istats[4] = { 0, static_cast<int>(STRAT0), static_cast<int>(STRAT1), dev };
+    int istats[4] = { static_cast<int>(routine), static_cast<int>(STRAT0), static_cast<int>(STRAT1), static_cast<int>(ret) };
     istats[0] = ((ret < 0) ? ret : static_cast<int>(glbSwp));
     dims[0] = static_cast<hsize_t>(4u);
     dims[1] = static_cast<hsize_t>(1u);
