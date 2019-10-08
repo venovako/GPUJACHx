201392a68f06afb035d51316b80a0274175d3515
Vedran Novakovic <venovako@venovako.eu>
2019-10-08 21:10:22 +0200

    Refresh the software versions.

--- 1/mkLNX.sh
+++ 1/mkLNX.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-make -f Makefile.LNX GPU_ARCH=$1 NDEBUG=$2 $3
+GCCMAJORVER=`gcc -dumpversion | cut -d. -f1`
+if [ "$GCCMAJORVER" != "4" ]
+then
+	make -f Makefile.LNX GPU_ARCH=$1 NDEBUG=$2 STD=14 $3
+else
+	make -f Makefile.LNX GPU_ARCH=$1 NDEBUG=$2 $3
+fi
+unset GCCMAJORVER
