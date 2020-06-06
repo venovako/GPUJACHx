dc3cddf7a113d03d2f12b262d3970bf13751ef51
Vedran Novakovic <venovako@venovako.eu>
2020-06-06 17:39:52 +0200

    Tweak for CUDA 11.

--- 1/mkLNX.sh
+++ 1/mkLNX.sh
@@ -2,7 +2,13 @@
 GCCMAJORVER=`gcc -dumpversion | cut -d. -f1`
 if [ "$GCCMAJORVER" != "4" ]
 then
-	make -f Makefile.LNX GPU_ARCH=$1 NDEBUG=$2 STD=14 $3
+	if [ "$GCCMAJORVER" != "9" ]
+	then
+		make -f Makefile.LNX GPU_ARCH=$1 NDEBUG=$2 STD=14 $3
+	else
+		make -f Makefile.LNX GPU_ARCH=$1 NDEBUG=$2 STD=17 $3
+	fi
+	
 else
 	make -f Makefile.LNX GPU_ARCH=$1 NDEBUG=$2 $3
 fi
